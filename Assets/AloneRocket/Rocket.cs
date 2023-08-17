e5469f2e6cd270fc319847ff15d6ab844f266ea6
LUF-FY <margiev.erik@list.ru>
2023-08-17 16:13:29 +0300

    Удалил ненужные библиотеки и добавил иконку

--- Assets/AloneRocket/Rocket.cs
+++ Assets/AloneRocket/Rocket.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
@@ -14,17 +11,6 @@ public class Rocket : MonoBehaviour
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public int GetScore()
